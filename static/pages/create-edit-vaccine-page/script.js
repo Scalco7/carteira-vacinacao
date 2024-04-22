6669cfee5375cd0a298b13320d2306546660b3d6
Scalco7 <fscalco7@gmail.com>
2024-04-22 20:44:28 -0300

    create-edit-page

--- static/pages/create-edit-vaccine-page/script.js
+++ static/pages/create-edit-vaccine-page/script.js
@@ -1,3 +1,22 @@
+const img = document.querySelector('#proof-img');
+
 function navigateToPage(page) {
     window.location.href = page;
+}
+
+function save() {
+    console.log("salvando");
+}
+
+function saveProof(evt) {
+    if (!(evt.target && evt.target.files && evt.target.files.length > 0)) {
+        return;
+    }
+
+    var r = new FileReader();
+    r.onload = function () {
+        img.src = r.result;
+    }
+
+    r.readAsDataURL(evt.target.files[0]);
 }
